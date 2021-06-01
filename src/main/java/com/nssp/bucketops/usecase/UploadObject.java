c71c6e001152edfc746aed0ff63db2b6009f97e4
felipejsm <felipejsm41@gmail.com>
2021-06-01 01:20:56 -0300

    CRUD testado

--- src/main/java/com/nssp/bucketops/usecase/UploadObject.java
+++ src/main/java/com/nssp/bucketops/usecase/UploadObject.java
@@ -1,11 +1,6 @@
 package com.nssp.bucketops.usecase;
 
-import software.amazon.awssdk.core.ResponseInputStream;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-
-import java.util.List;
-
 public interface UploadObject {
     public String save();
-    public
+    //public
 }
